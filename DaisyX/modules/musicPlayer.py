6c3a3e0815469744c44f5aaaa5cd38fd49358a88
uditvarshney100 <81468435+uditvarshney100@users.noreply.github.com>
2021-04-26 16:18:05 +0530

    Create musicPlayer.py

---
+++ DaisyX/modules/musicPlayer.py
@@ -0,0 +1,30 @@
+__mod_name__ = "Music Player"
+__help__ = """
+<b> VC PLAYER </b>
+VC player plays plays music in your group's voice chat
+<b> Setting up </b>
+1) Add @doraemonxassistant to your group
+2) Make bot admin
+3) Start a voice chat
+<b> Commands </b>
+/play  - play song you requested
+/dplay  - play song you requested via deezer
+/splay  - play song you requested via jio saavn
+/playlist - Show now playing list
+/current - Show now playing
+/song  - download songs you want quickly
+/search  - search videos on youtube with details
+/deezer  - download songs you want quickly via deezer
+/saavn  - download songs you want quickly via saavn
+/video  - download videos you want quickly
+
+*Admins only*
+/player - open music player settings panel
+/pause - pause song play
+/resume - resume song play
+/skip - play next song
+/end - stop music play
+/userbotjoin - invite assistant to your chat
+/admincache - Refresh admin list
+<b> PLEASE NOTE THIS IS HEAVILY UNSTABLE AND CAN BE STOPPED ANYTIME </b>
+"""
